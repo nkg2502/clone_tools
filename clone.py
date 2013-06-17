e3ea9ee59bd0e6f7653f55089fd6691c7e03e915
Taesung Jung <tjung@qti.qualcomm.com>
2013-06-17 10:47:56 +0900

    modified 	add PathDialog

--- clone.py
+++ clone.py
@@ -3,6 +3,57 @@ import os
 import glob
 from Tkinter import *
 
+class PathDialog:
+
+	def __init__(self, root):
+
+		root.geometry('1000x100')
+		root.resizable(0, 0)
+
+		# key bind
+		root.bind('<Return>', self.write_path_file)
+		root.bind('<Escape>', self.cancel)
+
+		frame = Frame(root)
+		frame.pack(fill=BOTH, expand=1)
+
+		path_entry = Entry(frame, font='Consolas 14')
+
+		path_entry.insert(0, self.read_path_file())
+		path_entry.focus_set()
+		path_entry.select_range(0, END)
+
+		path_entry.pack(side=TOP, fill=X)
+
+		Button(frame, text='Cancel', font='Consolas 10', command=self.cancel).pack(side=RIGHT)
+		Button(frame, text='  OK  ', font='Consolas 10', command=self.write_path_file).pack(side=RIGHT)
+
+		self.root = root
+		self.path_entry = path_entry
+
+		root.mainloop()
+	
+	def write_path_file(self, event=None):
+		path_file = open('path.txt', 'w')
+		path_file.write(self.path_entry.get())
+
+		self.cancel()
+	
+	def read_path_file(self):
+
+		path = ''
+
+		try:
+			path_file = open('path.txt')
+			path = path_file.readlines()[0]
+		except:
+			pass
+
+		return path
+
+	def cancel(self, event=None):
+		self.root.destroy()
+
 # use command 'net use'
 def connect_path(drive_letter, path):
 	cmd = 'net use {} {}'.format(drive_letter, path)
@@ -14,15 +65,14 @@ def disconnect_path(drive_letter):
 	os.system(cmd)
 
 def get_path():
+
 	path = ''
 	try:
 		path = open('path.txt').readline()
 	except:
 		pass
 
-	user_input = raw_input('path[{}]: '.format(path))
-
-	return user_input.rstrip() if user_input.rstrip() else path
+	return path
 
 def get_build_id(path):
 	return path.split('\\')[-1]
@@ -48,6 +98,8 @@ drive_letter = get_drive_letter()
 if None == drive_letter:
 	print 'Fatal: EVERY DRIVE LETTER USED'
 	sys.exit(1)
+
+PathDialog(Tk())
 
 path = get_path()
 if '' == path:
